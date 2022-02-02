fc5f79e4f9aa3af7b9dbbbd7da8e1cc43b2e1ed4
Bradley Reynolds <bradley.reynolds@darbia.dev>
2022-02-02 10:32:25 -0600

    Import all controls in controls init

--- pglet/__init__.py
+++ pglet/__init__.py
@@ -1,35 +1,3 @@
-from pglet.controls.control import Control
-from pglet.controls.page import Page
-from pglet.controls.stack import Stack
-from pglet.controls.text import Text
-from pglet.controls.textbox import Textbox
-from pglet.controls.checkbox import Checkbox
-from pglet.controls.progress import Progress
-from pglet.controls.button import Button
-from pglet.controls.dropdown import Dropdown
-from pglet.controls.html import Html
-from pglet.controls.icon import Icon
-from pglet.controls.iframe import IFrame
-from pglet.controls.link import Link
-from pglet.controls.toggle import Toggle
-from pglet.controls.message import Message, MessageButton
-from pglet.controls.choicegroup import ChoiceGroup
-from pglet.controls.nav import Nav
-from pglet.controls.searchbox import SearchBox
-from pglet.controls.slider import Slider
-from pglet.controls.spinbutton import SpinButton
-from pglet.controls.spinner import Spinner
-from pglet.controls.tabs import Tabs, Tab
-from pglet.controls.dialog import Dialog
-from pglet.controls.grid import Grid, Columns, Items, Column, Item
-from pglet.controls.panel import Panel
-from pglet.controls.toolbar import Toolbar
-from pglet.controls.verticalbarchart import VerticalBarChart
-from pglet.controls.barchart import BarChart
-from pglet.controls.callout import Callout
-from pglet.controls.image import Image
-from pglet.controls.linechart import LineChart
-from pglet.controls.piechart import PieChart
-from pglet.controls.datepicker import DatePicker
 from pglet.pglet import *
 from pglet.reconnecting_websocket import *
+from .controls import *
