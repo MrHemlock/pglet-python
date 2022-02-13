c936dba15e750e930fdf767bf4ce3c3111da7813
Christopher James <68414004+gamingbuddhist@users.noreply.github.com>
2022-02-13 09:53:06 -0500

    Updated connection.py with docstrings and type hints
    - Added file docstring
    - Added docstrings to every class and function in sphinx format
    - Added type hints where appropriate
    - Fixed != comparison to None: changed to "is not None"

--- pglet/connection.py
+++ pglet/connection.py
@@ -1,3 +1,7 @@
+""" Module for the Connection class. """
+
+from __future__ import annotations
+from typing import Callable, List, Optional
 import json
 import logging
 import threading
@@ -9,35 +13,67 @@ from pglet.reconnecting_websocket import ReconnectingWebSocket
 
 
 class Connection:
+    """ A connection to the pglet server. This class is used to connect to the pglet server
+    and send and receive
+
+    :param ws: The websocket to use for the connection.
+    :type ws: ReconnectingWebSocket
+    """
+
     def __init__(self, ws: ReconnectingWebSocket):
         self._ws = ws
         self._ws.on_message = self._on_message
-        self._ws_callbacks = {}
-        self._on_event = None
-        self._on_session_created = None
-        self.host_client_id = None
-        self.page_name = None
-        self.page_url = None
-        self.browser_opened = False
-        self.sessions = {}
+        self._ws_callbacks: dict = {}
+        self._on_event: Optional[Callable] = None
+        self._on_session_created: Optional[Callable] = None
+        self.host_client_id: str | None = None
+        self.page_name: str | None = None
+        self.page_url: str | None = None
+        self.browser_opened: bool = False
+        self.sessions: dict = {}
 
     @property
-    def on_event(self):
+    def on_event(self) -> Optional[Callable]:
+        """ The event handler. This is called when a new event is received.
+
+        :return: The event handler.
+        :rtype: Callable
+        """
         return self._on_event
 
     @on_event.setter
-    def on_event(self, handler):
+    def on_event(self, handler: Callable) -> None:
+        """ Set the event handler. This is called when a new event is received.
+
+        :param handler: The event handler.
+        :type handler: Callable
+        """
         self._on_event = handler
 
     @property
-    def on_session_created(self):
+    def on_session_created(self) -> Optional[Callable]:
+        """ The session created handler. This is called when a new session is created.
+
+        :return: The session created handler.
+        :rtype: Callable
+        """
         return self._on_session_created
 
     @on_session_created.setter
-    def on_session_created(self, handler):
+    def on_session_created(self, handler: Callable) -> None:
+        """ Set the session created handler. This is called when a new session is created.
+
+        :param handler: The session created handler.
+        :type handler: Callable
+        """
         self._on_session_created = handler
 
-    def _on_message(self, data):
+    def _on_message(self, data: str) -> None:
+        """ Called when a message is received. This is called by the websocket.
+
+        :param data: The message data.
+        :type data: str
+        """
         logging.debug(f"_on_message: {data}")
         msg_dict = json.loads(data)
         msg = Message(**msg_dict)
@@ -47,7 +83,7 @@ class Connection:
             self._ws_callbacks[msg.id] = (None, msg.payload)
             evt.set()
         elif msg.action == Actions.PAGE_EVENT_TO_HOST:
-            if self._on_event != None:
+            if self._on_event is not None:
                 th = threading.Thread(
                     target=self._on_event,
                     args=(
@@ -59,7 +95,7 @@ class Connection:
                 th.start()
                 # self._on_event(self, PageEventPayload(**msg.payload))
         elif msg.action == Actions.SESSION_CREATED:
-            if self._on_session_created != None:
+            if self._on_session_created is not None:
                 th = threading.Thread(
                     target=self._on_session_created,
                     args=(
@@ -81,14 +117,42 @@ class Connection:
         update: bool,
         auth_token: str,
         permissions: str,
-    ):
+    ) -> RegisterHostClientResponsePayload:
+        """ Register the host client.
+
+        :param host_client_id: The host client id.
+        :type host_client_id: str
+        :param page_name: The page name.
+        :type page_name: str
+        :param is_app: Whether the page is an app.
+        :type is_app: bool
+        :param update: Whether the page is an update.
+        :type update: bool
+        :param auth_token: The auth token.
+        :type auth_token: str
+        :param permissions: The permissions.
+        :type permissions: str
+        :return: The response payload.
+        :rtype: RegisterHostClientResponsePayload
+        """
         payload = RegisterHostClientRequestPayload(
             host_client_id, page_name, is_app, update, auth_token, permissions
         )
         response = self._send_message_with_result(Actions.REGISTER_HOST_CLIENT, payload)
         return RegisterHostClientResponsePayload(**response)
 
-    def send_command(self, page_name: str, session_id: str, command: Command):
+    def send_command(self, page_name: str, session_id: str, command: Command) -> PageCommandResponsePayload:
+        """ Send a command to the server.
+
+        :param page_name: The name of the page.
+        :type page_name: str
+        :param session_id: The session id.
+        :type session_id: str
+        :param command: The command to send.
+        :type command: Command
+        :return: The response payload.
+        :rtype: PageCommandResponsePayload
+        """
         payload = PageCommandRequestPayload(page_name, session_id, command)
         response = self._send_message_with_result(
             Actions.PAGE_COMMAND_FROM_HOST, payload
@@ -98,7 +162,23 @@ class Connection:
             raise Exception(result.error)
         return result
 
-    def send_commands(self, page_name: str, session_id: str, commands: List[Command]):
+    def send_commands(
+            self,
+            page_name: str,
+            session_id: str,
+            commands: List[Command]
+    ) -> PageCommandsBatchResponsePayload:
+        """ Send a list of commands to the server.
+
+        :param page_name: The name of the page.
+        :type page_name: str
+        :param session_id: The session id.
+        :type session_id: str
+        :param commands: The list of commands.
+        :type commands: List[Command]
+        :return: The response payload.
+        :rtype: PageCommandsBatchResponsePayload
+        """
         payload = PageCommandsBatchRequestPayload(page_name, session_id, commands)
         response = self._send_message_with_result(
             Actions.PAGE_COMMANDS_BATCH_FROM_HOST, payload
@@ -108,7 +188,20 @@ class Connection:
             raise Exception(result.error)
         return result
 
-    def _send_message_with_result(self, action_name, payload):
+    def _send_message_with_result(
+            self,
+            action_name: str,
+            payload: PageCommandsBatchRequestPayload | PageCommandRequestPayload | RegisterHostClientRequestPayload
+    ) -> dict:
+        """ Send a message to the host and wait for the response.
+
+        :param action_name: The action name.
+        :type action_name: str
+        :param payload: The payload.
+        :type payload: PageCommandsBatchRequestPayload | PageCommandRequestPayload | RegisterHostClientRequestPayload
+        :return: The response payload.
+        :rtype: dict
+        """
         msg_id = uuid.uuid4().hex
         msg = Message(msg_id, action_name, payload)
         j = json.dumps(msg, default=vars)
@@ -119,7 +212,8 @@ class Connection:
         evt.wait()
         return self._ws_callbacks.pop(msg_id)[1]
 
-    def close(self):
+    def close(self) -> None:
+        """ Close the connection. """
         logging.debug("Closing connection...")
-        if self._ws != None:
+        if self._ws is not None:
             self._ws.close()
